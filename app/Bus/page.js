ac93227cbf844688d7d578b00d583946dab4c2a8
EangJS <eugeneang654871@gmail.com>
2023-07-05 00:22:15 +0800

    restore bus stop

--- app/Bus/page.js
+++ app/Bus/page.js
@@ -30,22 +30,31 @@ const getData = async (BusStop) => {
 
     } catch (error) {
         console.error("Error:", error);
-        return {
-            props: {
-                data: [],
-            },
-        };
     }
 
 };
 
 export default async function Page({searchParams}) {
     const data = await getData(searchParams.BusStop);
-    var services = data.Services;
+    var services = await data.Services;
+    console.log(services);
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <>
+            <div className="pl-5 pt-2 mt-2">
+                <a className="bg-[#3a383e] col-[#cfbcf1] p-2 rounded-2xl" href="/">Back</a>
+            </div>
+
+            <div className={styles.main}>
+                <BusStop busStop={searchParams.BusStop}></BusStop>
+                <div className="flex justify-center gap-5 p-5 flex-wrap bg-[#262429] rounded-2xl m-5">
+                    {services.map((item, index) => (
+                        <BusData key={index}
+                                 Service={item}>
+                        </BusData>
+                    ))}
+                </div>
+            </div>
+        </>
     );
 
 }
