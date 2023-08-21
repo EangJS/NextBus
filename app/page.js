184720318b6130de97d5d4e820c3477ed8583b96
Eugene Ang <eugeneang@Eugenes-Mac-mini.local>
2023-08-21 19:47:03 -0400

    Add github link

--- app/page.js
+++ app/page.js
@@ -3,20 +3,20 @@ import Location from './NearestStops/Location';
 
 export default function Home() {
     return (
-        <main className={styles.main} style={{gap: '20px'}}>
+        <main className={styles.main} style={{ gap: '20px' }}>
             <Location></Location>
             <div className="bg-[#212529] rounded-2xl p-5 ml-10 mr-10 flex flex-wrap justify-center gap-5">
                 <div className="bg-[#243240] rounded-2xl p-5">
                     <h2 className="text-3xl font-bold p-3">Search</h2>
                     <div className="center-flex">
                         <form action="/Bus" method={"get"}
-                              className="flex flex-col justify-center gap-10">
+                            className="flex flex-col justify-center gap-10">
                             <div>
                                 <label htmlFor="BusStop">Bus Stop: </label>
                                 <input
                                     className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2
                         px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                                    type="number" id="BusStop" name="BusStop" placeholder="Bus Stop"/>
+                                    type="number" id="BusStop" name="BusStop" placeholder="Bus Stop" />
                             </div>
                             <button
                                 className="px-8 py-4 text-lg font-medium text-center text-white bg-[#4f378a] rounded-md
@@ -26,7 +26,7 @@ export default function Home() {
                         </form>
                     </div>
                 </div>
-                <div className="bg-[#243240] rounded-2xl p-5 flex flex-col justify-between">
+                <div className="bg-[#243240] hidden rounded-2xl p-5 flex-col justify-between">
                     <h2 className="text-3xl font-bold p-3">Checkpoint</h2>
                     <a className="px-8 py-4 text-lg font-medium text-center text-white bg-[#4f378a] rounded-md
                              transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:bg-indigo-500
@@ -34,6 +34,16 @@ export default function Home() {
 
                 </div>
             </div>
+            <hr className="w-full" />
+            <footer>
+                <div>
+                    <a href="https://github.com/EangJS/NextBus" className="flex items-center gap-3 text-blue-500">
+                        <p>Find the repository here</p>
+                        <img height="30px" width="30px" src="/github-mark.svg" />
+                    </a>
+                </div>
+
+            </footer>
 
         </main>
     )
